81d65a79563afba8070aa0329655a511a54d5e18
kaelky <rickychandra.j@gmail.com>
2018-10-14 21:46:30 +0700

    Initiation Setup Environment

---
+++ src/main/java/com/apap/tugas1_1606878013/Tugas11606878013Application.java
@@ -0,0 +1,12 @@
+package com.apap.tugas1_1606878013;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas11606878013Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tugas11606878013Application.class, args);
+	}
+}
