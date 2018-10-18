37acfe03ad34685d3598f4fff8559ad145c287c5
kaelky <rickychandra.j@gmail.com>
2018-10-18 15:39:12 +0700

    Initiate Pegawai JPARepository

--- src/main/java/com/apap/tugas1_1606878013/repository/PegawaiDb.java
+++ src/main/java/com/apap/tugas1_1606878013/repository/PegawaiDb.java
@@ -1,4 +1,10 @@
 package com.apap.tugas1_1606878013.repository;
 
-public interface PegawaiDb {
+import com.apap.tugas1_1606878013.model.PegawaiModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PegawaiDb extends JpaRepository<PegawaiModel, Long> {
+
 }
