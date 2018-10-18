23a940768a3a85b1bfb3b980e006e1d806dd3269
kaelky <rickychandra.j@gmail.com>
2018-10-18 15:40:38 +0700

    Setup JabatanPegawaiModel

--- src/main/java/com/apap/tugas1_1606878013/model/JabatanPegawaiModel.java
+++ src/main/java/com/apap/tugas1_1606878013/model/JabatanPegawaiModel.java
@@ -1,7 +1,11 @@
 package com.apap.tugas1_1606878013.model;
 
-import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity
@@ -9,5 +13,42 @@ import java.io.Serializable;
 public class JabatanPegawaiModel implements Serializable {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Size(max = 20)
     private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pegawai_id", referencedColumnName = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    @JsonIgnore
+    private long idPegawai;
+
+    @ManyToOne
+    @JoinColumn(name = "jabatan_id", referencedColumnName = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    @JsonIgnore
+    private long idJabatan;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getIdPegawai() {
+        return idPegawai;
+    }
+
+    public void setIdPegawai(long idPegawai) {
+        this.idPegawai = idPegawai;
+    }
+
+    public long getIdJabatan() {
+        return idJabatan;
+    }
+
+    public void setIdJabatan(long idJabatan) {
+        this.idJabatan = idJabatan;
+    }
 }
