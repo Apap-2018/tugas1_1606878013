b2a88aaac06a533a0c468f503d5e2306efc7b9e8
kaelky <rickychandra.j@gmail.com>
2018-10-18 15:41:27 +0700

    Setup InstansiModel

--- src/main/java/com/apap/tugas1_1606878013/model/InstansiModel.java
+++ src/main/java/com/apap/tugas1_1606878013/model/InstansiModel.java
@@ -1,7 +1,12 @@
 package com.apap.tugas1_1606878013.model;
 
-import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity
@@ -9,5 +14,54 @@ import java.io.Serializable;
 public class InstansiModel implements Serializable {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Size(max = 20)
     private long id;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "nama", nullable = false)
+    private String nama;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "deskripsi", nullable = false)
+    private String deskripsi;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "provinsi_id", referencedColumnName = "id")
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    @JsonIgnore
+    private long idProvinsi;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public long getIdProvinsi() {
+        return idProvinsi;
+    }
+
+    public void setIdProvinsi(long idProvinsi) {
+        this.idProvinsi = idProvinsi;
+    }
 }
