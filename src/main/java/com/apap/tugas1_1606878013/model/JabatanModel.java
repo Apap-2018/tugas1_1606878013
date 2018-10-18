3c31b021b88991d3b13f29855fb39370e74f106c
kaelky <rickychandra.j@gmail.com>
2018-10-18 15:41:10 +0700

    Setup JabatanModel

--- src/main/java/com/apap/tugas1_1606878013/model/JabatanModel.java
+++ src/main/java/com/apap/tugas1_1606878013/model/JabatanModel.java
@@ -1,7 +1,8 @@
 package com.apap.tugas1_1606878013.model;
 
-import javax.annotation.Generated;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity
@@ -9,5 +10,52 @@ import java.io.Serializable;
 public class JabatanModel implements Serializable {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Size(max = 20)
     private long id;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "jabatan", nullable = false)
+    private String nama;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "deskripsi", nullable = false)
+    private String deskripsi;
+
+    @NotNull
+    @Column(name = "gaji_pokok", nullable = false)
+    private Double gajiPokok;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public Double getGajiPokok() {
+        return gajiPokok;
+    }
+
+    public void setGajiPokok(Double gajiPokok) {
+        this.gajiPokok = gajiPokok;
+    }
 }
