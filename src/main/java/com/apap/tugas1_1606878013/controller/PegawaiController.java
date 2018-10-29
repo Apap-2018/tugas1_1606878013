31ad37929d0524692ad70e952cfdd315daa95beb
kaelky <rickychandra.j@gmail.com>
2018-10-29 12:15:11 +0700

    50% Fitur 2

--- src/main/java/com/apap/tugas1_1606878013/controller/PegawaiController.java
+++ src/main/java/com/apap/tugas1_1606878013/controller/PegawaiController.java
@@ -8,18 +8,22 @@ import com.apap.tugas1_1606878013.repository.InstansiDb;
 import com.apap.tugas1_1606878013.repository.JabatanDb;
 import com.apap.tugas1_1606878013.repository.PegawaiDb;
 import com.apap.tugas1_1606878013.repository.ProvinsiDb;
+import com.apap.tugas1_1606878013.service.InstansiService;
 import com.apap.tugas1_1606878013.service.PegawaiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class PegawaiController {
     @Autowired
     private PegawaiService pegawaiService;
+
+    @Autowired
+    private InstansiService instansiService;
 
     @Autowired
     private PegawaiDb pegawaiDb;
@@ -66,74 +70,92 @@ public class PegawaiController {
         List<ProvinsiModel> allProvinsi = provinsiDb.findAll();
         List<InstansiModel> allInstansi = instansiDb.findAll();
         List<JabatanModel> allJabatan = jabatanDb.findAll();
-        model.addAttribute("newPegawai", new PegawaiModel());
+
+        PegawaiModel pegawai = new PegawaiModel();
+        pegawai.setJabatanPegawai(new ArrayList<JabatanModel>());
+        pegawai.getJabatanPegawai().add(new JabatanModel());
+
+        model.addAttribute("pegawai", pegawai);
         model.addAttribute("allProvinsi", allProvinsi);
         model.addAttribute("allInstansi",  allInstansi);
         model.addAttribute("allJabatan", allJabatan);
         return "add_pegawai";
     }
 
-    /*@GetMapping(value = "/pegawai/tambah/submit")
-    private String add(@RequestParam("nama") String nama,
-                       @RequestParam("tempat_lahir") String tempatLahir,
-                       @RequestParam("tanggal_lahir") Date tanggalLahir,
-                       @RequestParam("tahun_masuk") String tahunMasuk,
-                       @RequestParam("provinsi") long idProvinsi,
-                       @RequestParam("instansi") long idInstansi,
-                       @RequestParam("jabatan") long idJabatan)
-    {
-        ProvinsiModel provinsi = provinsiDb.getOne(idProvinsi);
-        InstansiModel instansi = instansiDb.getOne(idInstansi);
-        JabatanModel jabatan = jabatanDb.getOne(idJabatan);
-        String tanggal = tanggalLahir.getDate() + "";
-        if ((tanggal + "").length() == 1){
-            tanggal =  ("0" + tanggal);
-        }
-        String bulan = tanggalLahir.getMonth() + 1 + "";
-        if ((bulan + "").length() == 1){
-            bulan = ("0" + bulan);
-        }
-        String tahun = (tanggalLahir.getYear() + "");
-        String nip = instansi.getId() + "" + tanggal + bulan + tahun + tahunMasuk;
-
-        List<PegawaiModel> pegawaiModels = pegawaiDb.findAll();
-        int nomorUrut = 1;
-        String tempNomorUrut = "";
-        for (PegawaiModel pegawai : pegawaiModels){
-            if (pegawai.getInstansi().getId() == instansi.getId() &&
-                    pegawai.getInstansi().getProvinsi().getId() == provinsi.getId() &&
-                    pegawai.getTahunMasuk() == tahunMasuk &&
-                    pegawai.getTanggalLahir() == tanggalLahir){
-                nomorUrut += 1;
-            }
-        }
-
-        if ((nomorUrut + "").length() == 1){
-            tempNomorUrut += "0" + nomorUrut;
-        }
-
-        nip += tempNomorUrut;
-        System.out.println(nip);
-
-        PegawaiModel newPegawai = new PegawaiModel();
-        newPegawai.setNip(nip);
-        newPegawai.setNama(nama);
-        newPegawai.setTempatLahir(tempatLahir);
-        newPegawai.setTanggalLahir(tanggalLahir);
-        newPegawai.setTahunMasuk(tahunMasuk);
-
-        InstansiModel instansiModel = instansiDb.getOne(idInstansi);
-        newPegawai.setInstansi(instansiModel);
-
-        ProvinsiModel provinsiModel = provinsiDb.getOne(idProvinsi);
-        newPegawai.getInstansi().setProvinsi(provinsiModel);
-
-        JabatanModel jabatanModel = jabatanDb.getOne(idJabatan);
-        newPegawai.addJabatan(jabatanModel);
-
-        pegawaiService.addPegawai(newPegawai);
-        return "add_pegawai_response";
+/*    @GetMapping(value = "/pegawai/instansi")
+    @ResponseBody
+    public List<InstansiModel> getInstansiByProvinsiId (@RequestParam(value = "provinsiId", required = true) long id){
+        ProvinsiModel provinsiModel = provinsiDb.getOne(id);
+        return instansiService.getInstansiByProvinsi(provinsiModel);
     }*/
+
+    @PostMapping(value = "/pegawai/tambah")
+    private String add(@ModelAttribute PegawaiModel pegawai, Model model){
+
+        int tempTanggal = pegawai.getTanggalLahir().getDate();
+        String tanggal = "";
+        if ((tempTanggal + "").length() == 1){
+            tanggal = "0" + tempTanggal;
+        }
+
+        int bulan = pegawai.getTanggalLahir().getMonth() + 1;
+        int tahun = pegawai.getTanggalLahir().getYear();
+//        String tahun = (tempTahun + "").substring(2,4);
+
+        long kodeInstansi = pegawai.getInstansi().getId();
+        String tahunMasuk = pegawai.getTahunMasuk();
+        List<PegawaiModel> seluruhPegawai = pegawaiDb.findByTahunMasukAndTanggalLahir(pegawai.getTahunMasuk(), pegawai.getTanggalLahir());
+
+
+        for (int i  =  0 ;  i < seluruhPegawai.size() ; i++){
+            System.out.println(seluruhPegawai.get(i).getNip());
+        }
+
+
+        Collections.sort(seluruhPegawai, new Comparator<PegawaiModel>() {
+            @Override
+            public int compare(PegawaiModel o1, PegawaiModel o2) {
+                long p1 = o1.getId();
+                long p2 = o2.getId();
+
+                if (p1 > p2) {
+                    return 1;
+                } else if (p1 < p2){
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+
+
+        String duaDigitTerakhir = "";
+        if (seluruhPegawai.size() == 0){
+            duaDigitTerakhir += "01";
+        }
+        else {
+            int tempNIPterakhir = Integer.parseInt(seluruhPegawai.get(seluruhPegawai.size()-1).getNip().substring(14,16));
+            tempNIPterakhir += 1;
+            if ((tempNIPterakhir + "").length() == 1){
+                duaDigitTerakhir += "0";
+            }
+            duaDigitTerakhir += tempNIPterakhir;
+        }
+//        int tempDuaDigitTerakhir = Integer.parseInt((seluruhPegawai.get((seluruhPegawai.size()-1)).getNip()));
+
+
+        /*tempDuaDigitTerakhir += 1;
+        if ((tempDuaDigitTerakhir+"").length() == 0){
+            duaDigitTerakhir += "0" + tempDuaDigitTerakhir;
+        }
+        else {
+            duaDigitTerakhir += tempDuaDigitTerakhir;
+        }*/
+        String nip = kodeInstansi + tanggal + bulan + tahun + tahunMasuk + duaDigitTerakhir;
+        pegawai.setNip(nip);
+        pegawaiService.addPegawai(pegawai);
+        return "add_pegawai_response";
+    }
 
     @GetMapping (value = "pegawai/termuda-tertua")
     private String seeOldestYoungestPegawai(@RequestParam("idInstansi") long idInstansi, Model model){
